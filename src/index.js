dcd5337ce0f9cae52c781f5dd86135190bfebee4
Chand-007 <bharathchandgetla@gmail.com>
2025-03-22 11:48:30 +0530

    testing charts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Barchart from './Barchart';
+import Barchart from './Barchart';//Testing charts for tracker
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
